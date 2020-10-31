921a09ed061beb17ee9b8ae096e688861904e223
Yova de León <erwingeo1021@gmail.com>
2020-10-31 14:35:19 -0600

    :tada: Commit inicial

---
+++ demo1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    __  __      __         __  ___                __    ");
+            Console.WriteLine("   / / / /___  / /___ _   /  |/  /_  ______  ____/ /___ ");
+            Console.WriteLine("  / /_/ / __ \\/ / __ `/  / /|_/ / / / / __ \\/ __  / __ \\");
+            Console.WriteLine(" / __  / /_/ / / /_/ /  / /  / / /_/ / / / / /_/ / /_/ /");
+            Console.WriteLine("/_/ /_/\\____/_/\\__,_/  /_/  /_/\\__,_/_/ /_/\\__,_/\\____/ ");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("  ______          __       ____                 ___   ____ ___   ____ ");
+            Console.WriteLine(" /_  __/__  _____/ /_     / __ \\____ ___  __   |__ \\ / __ \\__ \\ / __ \\");
+            Console.WriteLine("  / / / _ \\/ ___/ __ \\   / / / / __ `/ / / /   __/ // / / /_/ // / / / ");
+            Console.WriteLine(" / / /  __/ /__/ / / /  / /_/ / /_/ / /_/ /   / __// /_/ / __// /_/ / ");
+            Console.WriteLine("/_/  \\___/\\___/_/ /_/  /_____/\\__,_/\\__, /   /____/\\____/____/\\____/  ");
+            Console.WriteLine("                                   /____/                             ");
+        }
+    }
+}
